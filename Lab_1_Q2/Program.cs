1936b7aa6f99f161cd28e9c08e71234726cfd6ae
NandanVadi <nandanvadi@gmail.com>
2026-07-07 12:13:58 +0530

    completed lab1 q2

--- Lab_1_Q2/Program.cs
+++ Lab_1_Q2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = 25;
+            int guess;
+            int attempts = 0;
+
+            Console.WriteLine("=== Number Guessing Game ===");
+            Console.WriteLine("Guess the secret number between 1 and 50.");
+
+            do
+            {
+                Console.Write("Enter your guess: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                attempts++;
+
+                if (guess > secretNumber)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else if (guess < secretNumber)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed the correct number.");
+                    Console.WriteLine("Total attempts: " + attempts);
+                }
+
+            } while (guess != secretNumber);
+        }
+    }
+}
